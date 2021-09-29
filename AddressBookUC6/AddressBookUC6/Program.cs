74bb2dd4d047fe8a510942697b8a1337f7aa3d9f
naveen962 <mrn28897@gmail.com>
2021-09-29 04:14:45 -0700

    program to add multiple  addressbook is added

---
+++ AddressBookUC6/AddressBookUC6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AddressBookUC3
+{
+    class Program
+    {
+       
+        /// <summary>
+        /// Mains the specified arguments.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns></returns>
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Select the Addressbook  :   1->Addressbook1  2->Addressbook2");
+                int res = int.Parse(Console.ReadLine());
+                if (res == 1)
+                {
+                    AddressBookUC6.Addressbook1 ad1 = new AddressBookUC6.Addressbook1();
+                    ad1.getaddressbook1();
+
+                }
+                else if (res == 2)
+                {
+                    AddressBookUC6.Addressbook2 ad2 = new AddressBookUC6.Addressbook2();
+                    ad2.getaddressbook2();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+        }
+
+
+       
+    }
+}
