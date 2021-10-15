3f9a9558e254e43862268688f29912b30c0e1d83
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-15 00:37:20 -0700

    adding multiple address book

--- AddressBookUC6/AddressBookUC6/Program.cs
+++ AddressBookUC6/AddressBookUC6/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace AddressBookUC3
+namespace AddressBookUC6
 {
     class Program
     {
@@ -15,18 +15,23 @@ namespace AddressBookUC3
         {
             while (true)
             {
-                Console.WriteLine("Select the Addressbook  :   1->Addressbook1  2->Addressbook2");
+                Console.WriteLine("Select the Addressbook  :   1->Addressbook1  2->Addressbook2  3->Addressbook3");
                 int res = int.Parse(Console.ReadLine());
                 if (res == 1)
                 {
-                    AddressBookUC6.Addressbook1 ad1 = new AddressBookUC6.Addressbook1();
-                    ad1.getaddressbook1();
+                    Addressbook addressbook1 = new Addressbook();
+                    addressbook1.getaddressbook();
 
                 }
                 else if (res == 2)
                 {
-                    AddressBookUC6.Addressbook2 ad2 = new AddressBookUC6.Addressbook2();
-                    ad2.getaddressbook2();
+                    Addressbook addressbook2 = new Addressbook();
+                    addressbook2.getaddressbook();
+                }
+                else if (res == 3)
+                {
+                    Addressbook addressbook3 = new Addressbook();
+                    addressbook3.getaddressbook();
                 }
                 else
                 {
