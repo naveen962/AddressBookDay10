00e6f01de12fbd816a587a737c2e39c37f53356f
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-19 23:52:44 -0700

    program to search person by state or city

---
+++ Usecase8_Search_Person_By_State/Usecase8_Search_Person_By_State/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Usecase8_Search_Person_By_State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Welcome To Address Book System!");
+            Console.WriteLine("1.Enter to add the details");
+            Console.WriteLine("2.Enter to modify the details");
+            Console.WriteLine("3.Listing the details..");
+            Console.WriteLine("4.Remove the details");
+            Console.WriteLine("Enter a option");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    AddrBook addr1 = new AddrBook();
+                    addr1.GetCustomer("Naveeen", "Kumar", "8277284163", "smg", "shimoga", "Karnataka", "577203", "naveen@gmail.com");
+                    addr1.ListingPeople();
+                    break;
+                case "2":
+                    AddrBook addr2 = new AddrBook();
+                    addr2.GetCustomer("Naveeen", "Kumar", "8277284163", "smg", "shimoga", "Karnataka", "577203", "naveen@gmail.com");
+                    addr2.Modify();
+                    addr2.ListingPeople();
+                    break;
+                case "3":
+                    AddrBook addr3 = new AddrBook();
+                    addr3.GetCustomer("Naveeen", "Kumar", "8277284163", "smg", "shimoga", "Karnataka", "577203", "naveen@gmail.com");
+                    addr3.ListingPeople();
+                    break;
+                case "4":
+                    AddrBook addr4 = new AddrBook();
+                    addr4.GetCustomer("Naveeen", "Kumar", "8277284163", "smg", "shimoga", "Karnataka", "577203", "naveen@gmail.com");
+                    addr4.GetCustomer("Abhi", "Kumar", "8277284163", "smg", "shimoga", "Karnataka", "577203", "naveen@gmail.com");
+                    addr4.RemovePeople();
+
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+
+            }
+
+
+
+        }
+    }
+}
