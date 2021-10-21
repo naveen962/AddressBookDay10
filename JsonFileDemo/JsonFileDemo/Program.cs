0113fe386f671833050bfb71ea26f517b22b4f5a
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-21 01:27:25 -0700

    json file demo is added

---
+++ JsonFileDemo/JsonFileDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JsonFileDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CsvHandler.jsonImplementHandling();
+        }
+    }
+}
